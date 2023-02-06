784d285c94d44b96afa098122f42313b7cc8a48e
donglh <huudong.ptit@gmail.com>
2023-02-06 15:00:06 +0700

    update rollup terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,36 @@
 import babel from 'rollup-plugin-babel';
 import localResolve from 'rollup-plugin-local-resolve';
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 import strip from '@rollup/plugin-strip';
 
 export default {
   input: './src/index.js',
-  output: {
-    file: './dist/index.js',
-    format: 'umd',
-    name: 'index',
-    globals: {
-      react: 'React',
+  output: [
+    {
+      file: './dist/index.js',
+      format: 'umd',
+      name: 'redext',
+      globals: {
+        react: 'React'
+      }
+    },
+    {
+      file: './dist/cjs/index.js',
+      format: 'cjs',
+      name: 'redext',
+      globals: {
+        react: 'React'
+      }
+    },
+    {
+      file: './dist/esm/index.mjs',
+      format: 'esm',
+      name: 'redext',
+      globals: {
+        react: 'React'
+      }
     }
-  },
+  ],
   external: [
     'react'
   ],
@@ -33,7 +51,7 @@ export default {
       ]
     }),
     localResolve(),
-    uglify(),
+    terser(),
     strip()
   ]
 }
