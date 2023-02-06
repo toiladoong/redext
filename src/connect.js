5ad26a90251422dc964820c94e8134ff07ef08e7
doong <donglh@gviet.vn>
2023-02-06 15:10:27 +0700

    update connect

--- src/connect.js
+++ src/connect.js
@@ -1,33 +1,20 @@
 import React, { useContext } from 'react';
 import Context from './Context';
-import useDeepMemo from './hooks/useDeepMemo';
+import useContextSelector from './hooks/useContextSelector';
+import useDispatcher from './hooks/useDispatcher';
 
 const connect = (mapStateToProps, mapDispatchToProps) => Component => {
   return props => {
-    const { state = {}, dispatch, effects } = useContext(Context);
-
-    let filteredState = {};
-    let filteredDispatch = {};
-
-    if (mapStateToProps) {
-      filteredState = mapStateToProps(state);
-    } else {
-      filteredState = state
-    }
-
-    if (mapDispatchToProps) {
-      filteredDispatch = mapDispatchToProps(effects, dispatch)
-    }
-
-    const memoState = useDeepMemo(() => {
-      return filteredState
-    }, [filteredState]);
-
+    const { dispatch } = useContext(Context);
+    
+    const memoState = useContextSelector(mapStateToProps);
+    const dispatcher = useDispatcher(mapDispatchToProps);
+    
     return (
       <Component
         {...props}
         {...memoState}
-        {...filteredDispatch}
+        {...dispatcher}
         dispatch={dispatch}
       />
     )
